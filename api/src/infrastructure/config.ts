d7c155761d9d30bd6bad5bef0f31d595e8049110
Age <0800.grc@gmail.com>
2020-07-22 02:36:19 -0300

    Minor adjustments on config after database swap

--- api/src/infrastructure/config.ts
+++ api/src/infrastructure/config.ts
@@ -15,7 +15,7 @@ const config = {
   'port': getEnvVar('PORT', '8080'),
   'database': {
     'host': getEnvVar('DB_HOST', "localhost"),
-    'port': getEnvVar('DB_PORT', "3306"),
+    'port': getEnvVar('DB_PORT', "27017"),
     'username': getEnvVar('DB_USER', "development"),
     'password': getEnvVar('DB_PASSWORD', "devdatabase"),
     'database': getEnvVar('DB_NAME', "twitter-app"),
